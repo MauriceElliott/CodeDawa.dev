1ccb10b88abffa5f9be21a2596840a64431b063a
MauriceElliott <mauricemelliott@gmail.com>
2026-01-25 00:14:10 +0000

    fixup! fixup! fixup! fixup! Fixed issue with light and dark theme setter

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -12,22 +12,22 @@ struct Home: StaticPage {
             .class("float-end")
 
         Text{
-        "My name is "    
-        Strong("Maurice")
-        ", I am a "    
-        Strong("software engineer")
-        ", "
-        Strong("father")
-        ", "
-        Strong("creative")
-        ", and lover of all things sad and desperate. "
-        "<br>"
-        "This website is not a professional place, just somewhere I feel comfortable exposing a little of my inner being to the wider internet, in the hopes that it makes others feel normal in their own skin. At the same time it is for me mostly, and if I feel better after posting here, it has done its job."
-        "<br>"
-        Strong("Dawa")
-        " is arabic for medicine, or "
-        Strong("cure")
-        ". My implication with that is I found the cure to my addictions through code. Although saying that, it was definitely more my son being in the world that cured me."
+            "My name is "    
+            Strong("Maurice")
+            ", I am a "    
+            Strong("software engineer")
+            ", "
+            Strong("father")
+            ", "
+            Strong("creative")
+            ", and lover of all things sad and desperate. "
+            "<br>"
+            "This website is not a professional place, just somewhere I feel comfortable exposing a little of my inner being to the wider internet, in the hopes that it makes others feel normal in their own skin. At the same time it is for me mostly, and if I feel better after posting here, it has done its job."
+            "<br>"
+            Strong("Dawa")
+            " is arabic for medicine, or "
+            Strong("cure")
+            ". My implication with that is I found the cure to my addictions through code. Although saying that, it was definitely more my son being in the world that cured me."
         }
             .font(.body)
             .margin(.bottom, 100)
